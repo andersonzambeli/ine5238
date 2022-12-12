de966eb1ac0b2b2ba1a043c54bc4a6ed227e953a
andersonzambeli <88453454+andersonzambeli@users.noreply.github.com>
2022-12-12 09:29:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,29 @@ class Url():
     def __init__(self, nomeSite, endereco) -> None:
         self.nomeSite = nomeSite
         self.endereco = endereco
+        
+        
+        
+def writeJogosJson(jogos):
+        dicts_json = []
+        for j in jogos:
+            dict = {
+                
+                'titulo': j.titulo,
+                'disponibilidade': j.disponibilidade,
+                'numJogadores': j.numJogadores,
+                'idade': j.idade,
+                'tempoJogo': j.tempoJogo,
+                'editora': j.editora,
+                'siteFonte': j.siteFonte,
+                'preco': float(j.preco.removeprefix('R$ ').replace(',', '.'))
+
+            }
+            dicts_json.append(dict)
+        json_ = json.dumps(dicts_json , indent= 2, ensure_ascii=False)
+        print(json_)
+        return dicts_json
+
 
 
 urls =[ Url('Galapagos', 'https://www.mundogalapagos.com.br/'), Url('Play Easy', 'https://www.playeasy.com.br/')]
